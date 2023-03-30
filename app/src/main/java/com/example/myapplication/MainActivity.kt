457fea775be6d965d6cfe4208924857875a47f17
Alan Bignon <alan.bignon@universite-paris-saclay.fr>
2023-03-30 14:10:28 +0200

    note

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,23 +1,47 @@
 package com.example.myapplication
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    private val KEY_NOTE = "note"
+
+    private lateinit var noteEditText: EditText
+    private lateinit var saveButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val inputText = findViewById<EditText>(R.id.input_text)
-        val outputText = findViewById<TextView>(R.id.output_text)
-        val button = findViewById<Button>(R.id.button)
+        noteEditText = findViewById(R.id.note_edit_text)
+        saveButton = findViewById(R.id.save_button)
 
-        button.setOnClickListener {
-            val input = inputText.text.toString()
-            outputText.text = "You entered: $input"
+        // Load the previously saved note
+        val savedNote = getNoteFromSharedPreferences()
+        noteEditText.setText(savedNote)
+
+        // Save the note when the save button is clicked
+        saveButton.setOnClickListener {
+            val note = noteEditText.text.toString()
+            saveNoteToSharedPreferences(note)
+            Toast.makeText(this, "Note saved", Toast.LENGTH_SHORT).show()
         }
+    }
+
+    private fun getNoteFromSharedPreferences(): String {
+        val sharedPreferences = getSharedPreferences("MyNoteApp", Context.MODE_PRIVATE)
+        return sharedPreferences.getString(KEY_NOTE, "") ?: ""
+    }
+
+    private fun saveNoteToSharedPreferences(note: String) {
+        val sharedPreferences = getSharedPreferences("MyNoteApp", Context.MODE_PRIVATE)
+        val editor = sharedPreferences.edit()
+        editor.putString(KEY_NOTE, note)
+        editor.apply()
     }
 }
